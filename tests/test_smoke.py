a8005ec029db75b5aed5b0e254f1d7e29659c28a
Adam Coddington <me@adamcoddington.net>
2017-10-22 16:34:59 -0700

    Add an actual assertion here.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -18,17 +18,17 @@ class SmokeTest(BaseGUITestCase):
 
         self.save_screenshot()
         self.send_command('load /tmp/sample.gcode')
-        for _ in range(5):
-            self.save_screenshot()
-            time.sleep(1)
+        self.save_screenshot()
         pyautogui.press('f10')  # Mapped to 'start' in config
         for _ in range(10):
-            print self.get_bcnc_state()
             self.save_screenshot()
             time.sleep(1)
         pyautogui.press('f12')  # Mapped to 'stop' in config
         for _ in range(10):
-            print self.get_bcnc_state()
             self.save_screenshot()
             time.sleep(1)
         self.save_screenshot()
+
+        self.assertTrue(
+            'Alarm' in self.get_bcnc_state()['state']
+        )
